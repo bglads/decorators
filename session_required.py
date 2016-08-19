5d2eae13d6eaf1edf0b22bba3dd11fb07623e557
bglads <bhargav1015@yahoo.co.in>
2016-08-19 11:08:08 +0530

    django decorator to check for out-of-django session authentication

---
+++ session_required.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+import json
+
+def session_required(view_func):
+    def __session_required(request,*args, **kwargs):
+        try:
+            email=request.session['email']
+            if email is None:
+                raise ValueError('Cannot use None at session value')
+        except KeyError as e:
+            print str(e),"str(e)"
+            return HttpResponse(json.dumps({"status":'URL is invalid'}), content_type="application/json")
+        except ValueError as e:
+            print str(e),"str(e)"
+            return HttpResponse(json.dumps({"status":'URL is invalid'}), content_type="application/json")
+        else:
+            return view_func(request,*args, **kwargs)
+    return wraps(view_func)(__session_required)
